4505ccdc01d7b040083ac33e180972fa665bdabb
Håkon <144228803+HaakonStokkedal@users.noreply.github.com>
2024-03-01 10:52:52 +0100

    Add project files.

---
+++ Uppgift 6.6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.SymbolStore;
+
+namespace Uppgift_6._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in en mening:");
+            string svar = Console.ReadLine();
+            StoraSmå(svar);
+            Console.ReadKey();
+        }
+
+        static void StoraSmå(string s)
+        {
+            string[] strings = s.Split(' ');
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.Write(strings[i].ToLower());
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.Write(strings[i].ToUpper());
+                    Console.Write(" ");
+                }
+            }
+        }
+    }
+}
